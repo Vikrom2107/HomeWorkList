5cb4bab44a8dcfec9f08ac20196e03666d798136
Viktor Romanov <vikrom2107@yandex.ru>
2023-02-20 22:43:41 +0700

    Changed with methods

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,7 @@ public class Main {
                 break;
             }
             if ("1".equals(input) || "добавить".equals(input) ) {
-                System.out.println("Какую покупку хотите добавить? (Введите название)");
-                String product = scan.nextLine();
-                shopList.add(product);
-                System.out.println("Итого в списке покупок " + shopList.size());
-                System.out.println("*******************");
+                addProduct(shopList, scan);
             }
             if ("2".equals(input) || "показать".equals(input)) {
                 showList(shopList);
@@ -25,34 +21,52 @@ public class Main {
             }
             if ("3".equals(input) || "удалить".equals(input)) {
                 showList(shopList);
-                System.out.println("Какую покупку хотите удалить? Введите номер или название: ");
-                String deleteInput = scan.nextLine();
-                try {
-                    int number = Integer.parseInt(deleteInput);
-                    shopList.remove(number-1);
-                    System.out.println("*******************");
-                } catch (NumberFormatException e) {
-                    shopList.remove(deleteInput);
-                }
-                System.out.println("*******************");
+                deleteProduct(shopList, scan);
             }
             if ("4".equals(input) || "найти".equals(input)) {
-                System.out.println("Введите текст для поиска: ");
-                String find = scan.nextLine();
-                System.out.println("Найдено: ");
-                int findAmount = 0;
-                for (int i = 0; i < shopList.size(); i++) {
-                    if (shopList.get(i).toLowerCase().contains(find.toLowerCase())) {
-                        System.out.println(i+1 + ". " + shopList.get(i));
-                        findAmount += 1;
-                    }
-                }
-                if (findAmount == 0)
-                    System.out.println(findAmount);
-                System.out.println("*******************");
+                searchProducts(shopList, scan);
             }
         }
     }
+
+    private static void searchProducts(ArrayList<String> shopList, Scanner scan) {
+        System.out.println("Введите текст для поиска: ");
+        String find = scan.nextLine();
+        System.out.println("Найдено: ");
+        int findAmount = 0;
+        for (int i = 0; i < shopList.size(); i++) {
+            if (shopList.get(i).toLowerCase().contains(find.toLowerCase())) {
+                System.out.println(i+1 + ". " + shopList.get(i));
+                findAmount += 1;
+            }
+        }
+        if (findAmount == 0)
+            System.out.println(findAmount);
+        System.out.println("*******************");
+    }
+
+    private static void deleteProduct(ArrayList<String> shopList, Scanner scan) {
+        System.out.println("Какую покупку хотите удалить? Введите номер или название: ");
+        String deleteInput = scan.nextLine();
+        try {
+            int number = Integer.parseInt(deleteInput);
+            shopList.remove(number-1);
+            System.out.println("*******************");
+        } catch (NumberFormatException e) {
+            shopList.remove(deleteInput);
+            System.out.println("*******************");
+        }
+
+    }
+
+    private static void addProduct(ArrayList<String> shopList, Scanner scan) {
+        System.out.println("Какую покупку хотите добавить? (Введите название)");
+        String product = scan.nextLine();
+        shopList.add(product);
+        System.out.println("Итого в списке покупок " + shopList.size());
+        System.out.println("*******************");
+    }
+
     public static void showList(ArrayList<String> shopList) {
         System.out.println("Список покупок: ");
         for(int i = 0; i < shopList.size(); i++) {
